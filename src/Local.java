327858bcfb15773813f275be8f23e579c0618e8f
DenisLobas <denislobas.1620@icloud.com>
2025-10-31 23:31:22 +0300

    Обновил Local.java и Main.java

--- src/Local.java
+++ src/Local.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Local {
+
+    public static void main(String[] args) {
+
+        double price = 10.5;
+        double wallet = 100;
+        int unit = 3;
+
+
+        if (price * unit > wallet) {
+            System.out.println("Не достаточно денег");
+        } else if (price * unit < wallet || price * unit == wallet) {
+            System.out.println("Товар успешно приобретен");
+        }
+
+
+
+
+    }
 }
